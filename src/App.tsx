b8e748c7962926d055f559e65795f466cf228c2f
Cristina Hernández <achh17@gmail.com>
2023-05-05 17:53:40 -0500

    I added a public URL for routes.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Search from './components/Search';
 import SearchResults from "./components/SearchResults";
 import MovieDetail from "./components/MovieDetail";
+import env from './env';
 
 function App() {
   return (
-    <Router>
+    <Router basename={env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Search/>}/>
         <Route path="/results" element={<SearchResults />} />
